e16dbd1bd6348a2690892d86b1afe48d0a95a06d
Sridharalla <sridhar.alla@farmersedge.ca>
2018-02-14 10:30:50 -0500

    mean and std for ndvi

--- src/zoneNDVI.py
+++ src/zoneNDVI.py
@@ -60,6 +60,7 @@ def calcNdvi(red, nir):
 
 def generateNDVIPerZone(tiffFile, zoneMapFile):
     ndviZones = dict()
+    ndviCalcs = dict()
     zones = readTiff(zoneMapFile)
     original = readTiff(tiffFile)
     zoneArray = genZoneMasks(zones)
@@ -71,11 +72,14 @@ def generateNDVIPerZone(tiffFile, zoneMapFile):
     maskBandArr2 = genMaskedBand(original[2], zoneArray)
     maskBandArr3 = genMaskedBand(original[3], zoneArray)
 
-    tiffFileName = tiffFile.split('/')[2]
+    zoneMapFileName = zoneMapFile.split('/')[3]
+    tiffFileName = tiffFile.split('/')[2] + zoneMapFileName
+
     i = 0
     for x in range(len(zoneArray)-1):
         ndvi = calcNdvi(maskBandArr2[i], maskBandArr3[i])
         ndviZones[x] = ndvi
+        ndviCalcs[x] = (np.mean(ndvi), np.std(ndvi))
         meta = getMeta(tiffFile)
         meta.update(dtype=rasterio.uint16, count=1)
         with rasterio.open("../data/output/{}_ndvi{}.tiff".format(tiffFileName, i), 'w', **meta) as out:
@@ -94,24 +98,39 @@ def generateNDVIPerZone(tiffFile, zoneMapFile):
     plt.imsave("../data/output/{}_originalG.png".format(tiffFileName), original[1], cmap=plt.cm.summer)
     plt.imsave("../data/output/{}_originalR.png".format(tiffFileName), original[2], cmap=plt.cm.summer)
     plt.imsave("../data/output/{}_originalN.png".format(tiffFileName), original[3], cmap=plt.cm.summer)
-    return ndviZones
+
+    for k, v in ndviCalcs.items():
+        print k
+        pprint(v)
+
+    # for k,v in ndviZones.items():
+    #    print k
+    #    pprint(v)
+
+    return (ndviCalcs, ndviZones)
 
 
 zoneMapFile = "../data/(controller)_2017-08-30-2017-09-05__274501__Harvest/2017-08-30-2017-09-05__274502__Harvest__ZoneMap.tiff"
 tiffFile = "../data/20170217_215326_0c22_1B_AnalyticMS_274501.tiff"
 
-ndviZones = generateNDVIPerZone(tiffFile, zoneMapFile)
-for k,v in ndviZones.items():
-    print k
-    pprint(v)
+#(ndviCalcs, ndviZones) = generateNDVIPerZone(tiffFile, zoneMapFile)
 
 zoneMapFile = "../data/(controller)_2017-09-13-2017-09-28__297113__Harvest/2017-09-13-2017-09-28__297114__Harvest__ZoneMap.tiff"
+zoneMapFile2 = "../data/(controller)_2017-09-13-2017-09-28__297113__Harvest/2017-09-13-2017-09-28__297115__Harvest__ZoneMap.tiff"
+zoneMapFile3 = "../data/(controller)_2017-09-13-2017-09-28__297113__Harvest/2017-09-13-2017-09-28__428989__Harvest__ZoneMap.tiff"
 tiffFile = "../data/20170223_165518_0e30_1B_AnalyticMS_297113.tiff"
 
-ndviZones = generateNDVIPerZone(tiffFile, zoneMapFile)
-for k,v in ndviZones.items():
-    print k
-    pprint(v)
+print zoneMapFile
+#(ndviCalcs, ndviZones) = generateNDVIPerZone(tiffFile, zoneMapFile)
+
+print zoneMapFile2
+
+(ndviCalcs2, ndviZones) = generateNDVIPerZone(tiffFile, zoneMapFile2)
+
+print zoneMapFile3
+#(ndviCalcs3, ndviZones) = generateNDVIPerZone(tiffFile, zoneMapFile3)
+
+
 pprint("Completed")
 
 
